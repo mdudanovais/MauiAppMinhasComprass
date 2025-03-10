0e3614fd107275566baa9f4894c39fcb52380dd5
iago02770 <iagodt54@gmail.com>
2025-03-09 22:14:54 -0300

    Adição da Funcionalidade de Inseração no SQLite

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace MauiApp1
+﻿using MauiAppMinhasComprass.Helpers;
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras_db3");
+
+                    _db = new SQLiteDatabaseHelper("... db3");
+                }
+
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
